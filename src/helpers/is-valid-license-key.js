9e9f8774c9f03eb8f2535306bceec668dc878da3
Quang Lam <quang.lam2807@gmail.com>
2019-11-05 18:39:27 -0600

    Improve license validation (#15)

--- src/helpers/is-valid-license-key.js
+++ src/helpers/is-valid-license-key.js
@@ -2,14 +2,15 @@ import md5 from 'blueimp-md5';
 
 const isValidLicenseKey = (licenseKey) => {
   try {
-    const inputLicenseKey = licenseKey;
+    const inputLicenseKey = licenseKey.trim();
 
     const parts = inputLicenseKey.split('-');
     const quantity = parts[0];
     const time = parts[1];
     const md5Str = parts.slice(2).join('');
 
-    return md5(process.env.REACT_APP_LICENSE_SECRET + quantity + time).toUpperCase() === md5Str;
+    return md5(process.env.REACT_APP_LICENSE_SECRET + quantity + time).toUpperCase()
+      === md5Str.toUpperCase();
   } catch (err) {
     return false;
   }
