d05974e0dd9521282d224dd8d2c2024497ca90ca
Quang Lam <quang.lam2807@gmail.com>
2020-03-29 17:35:55 +0700

    Fix ICO icon is retrieved from Guardian instead of PNG icon (#178)

--- public/libs/get-website-icon-url-async.js
+++ public/libs/get-website-icon-url-async.js
@@ -20,9 +20,12 @@ const getWebsiteIconUrlAsync = (websiteURL) => customizedFetch(websiteURL)
       // find the icon with largest size
       rootElm.each((i, _elm) => {
         const elm = $(_elm);
-        // make sure icon is png
-        if (elm.attr('type') === 'image/png'
-          || elm.attr('href').endsWith('.png')) {
+        // check if type is PNG
+        // also check to make sure links doesn't end with .ico
+        // as some websites specify icon type wrong
+        // see https://github.com/atomery/webcatalog/issues/630 for more details
+        if ((elm.attr('type') === 'image/png' && !elm.attr('href').endsWith('.ico'))
+          || elm.attr('href').endsWith('.png')) { // if type is not specified but link ends with .png then assumes that the icon is PNG
           const size = elm.attr('sizes') ? parseInt(elm.attr('sizes').split('x'), 10) : 0;
           if (size >= maxSize) {
             maxSize = size;
