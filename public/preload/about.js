f8a1e9edfdd1538012050b85336a6d3c25e57318
Quang Lam <quang.lam2807@gmail.com>
2020-04-06 17:40:38 +0700

    Fix icon in about window is blurry (#185)

--- public/preload/about.js
+++ public/preload/about.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 window.mode = 'about';
 
-window.iconPath = path.join(__dirname, '..', 'icon.png');
+window.iconPath = path.join(__dirname, '..', 'icon@5x.png');
 
 const { remote } = require('electron');
 const contextMenu = require('electron-context-menu');
