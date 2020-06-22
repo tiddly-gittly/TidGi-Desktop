8478488d615525a3a39b658fe18e01c32828652d
Lin Onetwo <linonetwo012@gmail.com>
2020-06-23 00:09:25 +0800

    chore: add template wiki

---
+++ template/wiki/public/service-worker.js
@@ -0,0 +1,42 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.3/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉Service Worker is working!`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬Service Worker won't work properly...`);
+}
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst, StaleWhileRevalidate } = workbox.strategies;
+const { ExpirationPlugin } = workbox.expiration;
+const { precacheAndRoute, matchPrecache } = workbox.precaching;
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+  /\.css$/,
+  // Use cache but update in the background.
+  new StaleWhileRevalidate({
+    // Use a custom cache name.
+    cacheName: 'css-cache',
+  })
+);
+
+registerRoute(
+  /\.(?:png|jpg|jpeg|svg|gif|woff2?|ttf)$/,
+  // Use the cache if it's available.
+  new CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new ExpirationPlugin({
+        // Cache only a few images.
+        maxEntries: 100,
+        // Cache for a maximum of a week.
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+registerRoute(/\.js$/, new StaleWhileRevalidate());
+registerRoute(/(^\/$|index.html)/, new StaleWhileRevalidate());
