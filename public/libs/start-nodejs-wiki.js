c211d1aab184abe83534b2e3ccfc328c613b1dac
Lin Onetwo <linonetwo012@gmail.com>
2020-06-26 18:45:36 +0800

    fix: inform user when folder can't be find

--- public/libs/start-nodejs-wiki.js
+++ public/libs/start-nodejs-wiki.js
@@ -1,5 +1,9 @@
 const path = require('path');
+const fs = require('fs-extra');
+const { dialog } = require('electron');
 const $tw = require('tiddlywiki/boot/boot.js').TiddlyWiki();
+
+const mainWindow = require('../windows/main');
 
 const tiddlyWikiPort = 5112;
 const userName = 'LinOnetwoTest';
@@ -10,9 +14,27 @@ module.exports = function startNodeJSWiki(homePath) {
     return;
   }
   if (!homePath || typeof homePath !== 'string' || !path.isAbsolute(homePath)) {
-    console.error(
-      `valie absolute homePath not provided to startNodeJSWiki(homePath), received ${homePath} which is invalid.`
-    );
+    const errorMessage = `需要传入正确的路径，而 ${homePath} 无法被 TiddlyWiki 识别。`;
+    console.error(errorMessage);
+    dialog.showMessageBox(mainWindow.get(), {
+      title: '传入的路径无法使用',
+      message: errorMessage,
+      buttons: ['OK'],
+      cancelId: 0,
+      defaultId: 0,
+    });
+    return;
+  }
+  if (!fs.pathExistsSync(homePath)) {
+    const errorMessage = `无法找到之前还在该处的工作区文件夹！本应存在于 ${homePath} 的文件夹可能被移走了！`;
+    console.error(errorMessage);
+    dialog.showMessageBox(mainWindow.get(), {
+      title: '工作区文件夹被移走',
+      message: errorMessage,
+      buttons: ['OK'],
+      cancelId: 0,
+      defaultId: 0,
+    });
     return;
   }
 
