e3362ce4723bc5a0034b0753d3f99d2962ab9c8f
tiddlygit-test <linonetwo012@gmail.com>
2020-08-03 22:51:37 +0800

    doc: translation for wiki creation

--- public/libs/wiki/create-wiki.js
+++ public/libs/wiki/create-wiki.js
@@ -1,9 +1,11 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 const fs = require('fs-extra');
 const path = require('path');
 
 const { TIDDLYWIKI_TEMPLATE_FOLDER_PATH, TIDDLERS_PATH } = require('../../constants/paths');
 const { clone } = require('../git');
 const { logger } = require('../log');
+const i18n = require('../i18n');
 
 const logProgress = message =>
   logger.notice({
@@ -21,30 +23,30 @@ async function linkWiki(mainWikiPath, folderName, subWikiPath) {
   const mainWikiTiddlersFolderPath = path.join(mainWikiPath, TIDDLERS_PATH, folderName);
   try {
     await fs.createSymlink(subWikiPath, mainWikiTiddlersFolderPath);
-    logProgress(`从${subWikiPath}到${mainWikiTiddlersFolderPath}的链接创建成功，将文件存入后者相当于将文件存入前者。`);
+    logProgress(i18n.t('AddWorkspace.CreateLinkFromSubWikiToMainWikiSucceed'));
   } catch {
-    throw new Error(`无法链接文件夹 "${subWikiPath}" 到 "${mainWikiTiddlersFolderPath}"`);
+    throw new Error(i18n.t('AddWorkspace.CreateLinkFromSubWikiToMainWikiFailed', { subWikiPath, mainWikiTiddlersFolderPath }));
   }
 }
 
 async function createWiki(newFolderPath, folderName) {
-  logProgress('开始用模板创建Wiki');
+  logProgress(i18n.t('AddWorkspace.StartUsingTemplateToCreateWiki'));
   const newWikiPath = path.join(newFolderPath, folderName);
   if (!(await fs.pathExists(newFolderPath))) {
-    throw new Error(`该目录不存在 "${newFolderPath}"`);
+    throw new Error(i18n.t('AddWorkspace.PathNotExist', { newFolderPath }));
   }
   if (!(await fs.pathExists(TIDDLYWIKI_TEMPLATE_FOLDER_PATH))) {
-    throw new Error(`Wiki模板缺失 "${TIDDLYWIKI_TEMPLATE_FOLDER_PATH}"`);
+    throw new Error(i18n.t('AddWorkspace.WikiTemplateMissing', { TIDDLYWIKI_TEMPLATE_FOLDER_PATH }));
   }
   if (await fs.pathExists(newWikiPath)) {
-    throw new Error(`Wiki已经存在于该位置 "${newWikiPath}"`);
+    throw new Error(i18n.t('AddWorkspace.WikiExisted', { newWikiPath }));
   }
   try {
     await fs.copy(TIDDLYWIKI_TEMPLATE_FOLDER_PATH, newWikiPath);
   } catch {
-    throw new Error(`无法在该处创建文件夹 "${newWikiPath}"`);
+    throw new Error(i18n.t('AddWorkspace.CantCreateFolderHere', { newWikiPath }));
   }
-  logProgress('模板Wiki复制完毕');
+  logProgress(i18n.t('AddWorkspace.WikiTemplateCopyCompleted'));
 }
 
 /**
@@ -55,13 +57,13 @@ async function createWiki(newFolderPath, folderName) {
  * @param {boolean} onlyLink not creating new subwiki folder, just link existed subwiki folder to main wiki folder
  */
 async function createSubWiki(newFolderPath, folderName, mainWikiToLink, onlyLink = false) {
-  logProgress('开始创建子Wiki');
+  logProgress(i18n.t('AddWorkspace.StartCreatingSubWiki'));
   const newWikiPath = path.join(newFolderPath, folderName);
   if (!(await fs.pathExists(newFolderPath))) {
-    throw new Error(`该目录不存在 "${newFolderPath}"`);
+    throw new Error(i18n.t('AddWorkspace.PathNotExist', { newFolderPath }));
   }
   if (await fs.pathExists(newWikiPath)) {
-    throw new Error(`Wiki已经存在于该位置 "${newWikiPath}"`);
+    throw new Error(i18n.t('AddWorkspace.WikiExisted', { newWikiPath }));
   }
   logProgress('开始链接子Wiki到父Wiki');
   await linkWiki(mainWikiToLink, folderName, newWikiPath);
@@ -69,11 +71,11 @@ async function createSubWiki(newFolderPath, folderName, mainWikiToLink, onlyLink
     try {
       await fs.mkdirs(newWikiPath);
     } catch {
-      throw new Error(`无法在该处创建文件夹 "${newWikiPath}"`);
+      throw new Error(i18n.t('AddWorkspace.CantCreateFolderHere', { newWikiPath }));
     }
   }
 
-  logProgress('子Wiki创建完毕');
+  logProgress(i18n.t('AddWorkspace.SubWikiCreationCompleted'));
 }
 
 async function removeWiki(wikiPath, mainWikiToUnLink) {
@@ -86,38 +88,38 @@ async function removeWiki(wikiPath, mainWikiToUnLink) {
 
 async function ensureWikiExist(wikiPath, shouldBeMainWiki) {
   if (!(await fs.pathExists(wikiPath))) {
-    throw new Error(`该目录不存在 "${wikiPath}"`);
+    throw new Error(i18n.t('AddWorkspace.PathNotExist', { newFolderPath: wikiPath }));
   }
   if (shouldBeMainWiki && !(await fs.pathExists(path.join(wikiPath, TIDDLERS_PATH)))) {
-    throw new Error(`该目录不是一个Wiki文件夹 "${wikiPath}"`);
+    throw new Error(i18n.t('AddWorkspace.ThisPathIsNotAWikiFolder', { wikiPath }));
   }
 }
 
 async function cloneWiki(parentFolderLocation, wikiFolderName, githubWikiUrl, userInfo) {
-  logProgress('开始克隆Wiki');
+  logProgress(i18n.t('AddWorkspace.StartCloningWiki'));
   const newWikiPath = path.join(parentFolderLocation, wikiFolderName);
   if (!(await fs.pathExists(parentFolderLocation))) {
-    throw new Error(`该目录不存在 "${parentFolderLocation}"`);
+    throw new Error(i18n.t('AddWorkspace.PathNotExist', { newFolderPath: parentFolderLocation }));
   }
   if (await fs.pathExists(newWikiPath)) {
-    throw new Error(`Wiki已经存在于该位置 "${newWikiPath}"`);
+    throw new Error(i18n.t('AddWorkspace.WikiExisted', { newWikiPath }));
   }
   try {
     await fs.mkdir(newWikiPath);
   } catch {
-    throw new Error(`无法在该处创建文件夹 "${newWikiPath}"`);
+    throw new Error(i18n.t('AddWorkspace.CantCreateFolderHere', { newWikiPath }));
   }
   await clone(githubWikiUrl, path.join(parentFolderLocation, wikiFolderName), userInfo);
 }
 
 async function cloneSubWiki(parentFolderLocation, wikiFolderName, mainWikiPath, githubWikiUrl, userInfo) {
-  logProgress('开始克隆子Wiki');
+  logProgress(i18n.t('AddWorkspace.StartCloningSubWiki'));
   const newWikiPath = path.join(parentFolderLocation, wikiFolderName);
   if (!(await fs.pathExists(parentFolderLocation))) {
-    throw new Error(`该目录不存在 "${parentFolderLocation}"`);
+    throw new Error(i18n.t('AddWorkspace.PathNotExist', { newFolderPath: parentFolderLocation }));
   }
   if (await fs.pathExists(newWikiPath)) {
-    throw new Error(`Wiki已经存在于该位置 "${newWikiPath}"`);
+    throw new Error(i18n.t('AddWorkspace.WikiExisted', { newWikiPath }));
   }
   await clone(githubWikiUrl, path.join(parentFolderLocation, wikiFolderName), userInfo);
   await linkWiki(mainWikiPath, wikiFolderName, path.join(parentFolderLocation, wikiFolderName));
