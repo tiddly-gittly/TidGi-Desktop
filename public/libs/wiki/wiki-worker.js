d4b9897f6fb9006df73d82ce8921e5a7b8509c78
tiddlygit-test <linonetwo012@gmail.com>
2020-08-08 18:51:33 +0800

    feat: remove bob related wiki worker setup

--- public/libs/wiki/wiki-worker.js
+++ public/libs/wiki/wiki-worker.js
@@ -1,65 +1,26 @@
 const { workerData, parentPort, isMainThread } = require('worker_threads');
-const fs = require('fs');
 const path = require('path');
 const $tw = require('tiddlywiki').TiddlyWiki();
 
-async function startNodeJSWiki() {
+function startNodeJSWiki() {
   const { homePath, tiddlyWikiPort = 5112 } = workerData;
-  const bobServerConfigFolderPath = path.join(homePath, 'settings');
-  const bobServerConfigPath = path.join(bobServerConfigFolderPath, 'settings.json');
-  try {
-    if (
-      await fs.promises
-        .access(bobServerConfigPath, fs.constants.F_OK)
-        .then(() => true)
-        .catch(() => false)
-    ) {
-      await fs.promises.unlink(bobServerConfigPath);
-      await fs.promises.rmdir(bobServerConfigFolderPath);
-    }
-    await fs.promises.mkdir(bobServerConfigFolderPath);
-  } catch (error) {
-    parentPort.postMessage(`Configuring TW-Bob config folder failed with error ${error.message} ${error.stack}`);
-  }
-  try {
-    const bobServerConfig = {
-      serverName: 'TiddlyGit-Desktop',
-      persistentUsernames: 'yes',
-      heartbeat: {
-        interval: 200,
-        timeout: 1000,
-      },
-      enableBobSaver: 'no',
-      enableSaver: 'no',
-      saver: {
-        disable: 'yes',
-      },
-      pluginsPath: './plugins',
-      themesPath: './themes',
-      editionsPath: './',
-      wikiPathBase: homePath,
-      'ws-server': {
-        port: tiddlyWikiPort,
-        host: '0.0.0.0',
-        autoIncrementPort: false,
-        rootTiddler: '$:/core/save/lazy-images',
-      },
-    };
-    await fs.promises.writeFile(bobServerConfigPath, JSON.stringify(bobServerConfig, undefined, '  '));
-  } catch (error) {
-    parentPort.postMessage(`Configuring TW-Bob failed with error ${error.message} ${error.stack}`);
-  }
   try {
     process.env.TIDDLYWIKI_PLUGIN_PATH = path.resolve(homePath, 'plugins');
     process.env.TIDDLYWIKI_THEME_PATH = path.resolve(homePath, 'themes');
     // add tiddly filesystem back https://github.com/Jermolene/TiddlyWiki5/issues/4484#issuecomment-596779416
     $tw.boot.argv = [
-      '+plugins/OokTech/Bob',
+      '+plugins/tiddlywiki/filesystem',
+      '+plugins/tiddlywiki/tiddlyweb',
       homePath,
-      '--wsserver',
+      '--listen',
+      // `anon-username=${userName}`,
+      `port=${tiddlyWikiPort}`,
+      'host=0.0.0.0',
+      'root-tiddler=$:/core/save/lazy-images',
     ];
     $tw.boot.boot(() => parentPort.postMessage(`Tiddlywiki booted at http://localhost:${tiddlyWikiPort}`));
   } catch (error) {
+    console.error(error);
     parentPort.postMessage(`Tiddlywiki booted failed with error ${error.message} ${error.stack}`);
   }
 }
