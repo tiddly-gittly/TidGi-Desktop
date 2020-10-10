7c593c59167ede18d7b601d50dfd2f52bee61bec
tiddlygit-test <linonetwo012@gmail.com>
2020-10-11 00:42:21 +0800

    fix: new usage of authing

--- public/preload/add-workspace.js
+++ public/preload/add-workspace.js
@@ -25,8 +25,10 @@ setTimeout(refresh, CHECK_LOADED_INTERVAL);
 window.addEventListener(
   'message',
   event => {
-    if (typeof event?.data?.code === 'number' && event?.data?.data?.token) {
-      window.postMessage(event.data, '*');
+    if (typeof event?.data?.code === 'number' && event?.data?.data?.token && event?.data.from !== 'preload') {
+      // This message will be catch by this handler again, so we add a 'from' to indicate that it is re-send by ourself
+      // we re-send this, so authing in this window can catch it
+      window.postMessage({ ...event.data, from: 'preload' }, '*');
     }
   },
   false,
