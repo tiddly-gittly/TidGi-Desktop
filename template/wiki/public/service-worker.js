6c37677c2a23f2bc3cc2cd9deeb294510de03681
tiddlygit-test <linonetwo012@gmail.com>
2020-10-16 22:27:28 +0800

    feat: add service-worker plugin

--- template/wiki/public/service-worker.js
+++ template/wiki/public/service-worker.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.3/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js');
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉Service Worker is working!`);
