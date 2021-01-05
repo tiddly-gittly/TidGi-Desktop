0363b896d1e12d7fd68b2d78fa608d42a966910f
tiddlygit-test <linonetwo012@gmail.com>
2021-01-05 23:26:27 +0800

    fix: use service in preload

--- src/services/preload/wiki-git-api.ts
+++ src/services/preload/wiki-git-api.ts
@@ -3,16 +3,25 @@
  * This file should be required by BrowserView's preload script to work
  */
 import { contextBridge } from 'electron';
-import { getModifiedFileList } from '../git/inspect';
-import { commitAndSync } from '../git/sync';
-import { getWorkspacesAsList } from '../workspaces';
-import { getPreference } from '../preferences';
+
+import { container } from '@/services/container';
+import { getModifiedFileList } from '@services/git/inspect';
+import { Git } from '@services/git';
+import { Workspace } from '@services/workspaces';
+import { Authentication } from '@services/auth';
 
 contextBridge.exposeInMainWorld('git', {
   getModifiedFileList,
-  commitAndSync: (wikiPath: any, githubRepoUrl: any) => {
-    const userInfo = getPreference('github-user-info');
-    return commitAndSync(wikiPath, githubRepoUrl, userInfo);
+  commitAndSync: (wikiPath: string, githubRepoUrl: string) => {
+    const gitService = container.resolve(Git);
+    const authService = container.resolve(Authentication);
+    const userInfo = authService.get('authing');
+    if (userInfo !== undefined) {
+      return gitService.commitAndSync(wikiPath, githubRepoUrl, userInfo);
+    }
   },
-  getWorkspacesAsList,
+  getWorkspacesAsList: () => {
+    const workspaceService = container.resolve(Workspace);
+    return workspaceService.getWorkspacesAsList();
+  },
 });
