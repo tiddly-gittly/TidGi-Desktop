c2282b7ed46c3d6b8da259df48fab725d7dd1142
tiddlygit-test <linonetwo012@gmail.com>
2021-02-13 16:48:31 +0800

    feat: use ts-import-plugin for lodash and material-ui

--- webpack.rules.js
+++ webpack.rules.js
@@ -1,3 +1,5 @@
+const tsImportPluginFactory = require('ts-import-plugin');
+
 module.exports = [
   // Add support for native node modules
   {
@@ -37,6 +39,62 @@ module.exports = [
         options: {
           name: '[name].[ext]',
           outputPath: 'fonts/',
+          getCustomTransformers: () => ({
+            before: [
+              // lodash
+              tsImportPluginFactory({
+                style: false,
+                libraryName: 'lodash',
+                libraryDirectory: null,
+                camel2DashComponentName: false,
+              }),
+              // material-ui
+              transformerFactory({
+                libraryName: '@material-ui/core',
+                libraryDirectory: '',
+                camel2DashComponentName: false,
+              }),
+              // svg-icons
+              transformerFactory({
+                libraryDirectory: (importName) => {
+                  const stringVec = importName
+                    .split(/([A-Z][a-z]+|[0-9]*)/)
+                    .filter((s) => s.length)
+                    .map((s) => s.toLocaleLowerCase());
+
+                  return stringVec.reduce((acc, cur, index) => {
+                    if (index > 1) {
+                      return acc + '-' + cur;
+                    } else if (index === 1) {
+                      return acc + '/' + cur;
+                    }
+                    return acc + cur;
+                  }, '');
+                },
+                libraryName: '@material-ui/icons',
+                style: false,
+                camel2DashComponentName: false,
+              }),
+              // RXJS
+              transformerFactory([
+                {
+                  libraryDirectory: '../_esm5/internal/operators',
+                  libraryName: 'rxjs/operators',
+                  camel2DashComponentName: false,
+                  transformToDefaultImport: false,
+                },
+                {
+                  libraryDirectory: '../_esm5/internal/observable',
+                  libraryName: 'rxjs',
+                  camel2DashComponentName: false,
+                  transformToDefaultImport: false,
+                },
+              ]),
+            ],
+          }),
+          compilerOptions: {
+            module: 'es2015',
+          },
         },
       },
     ],
