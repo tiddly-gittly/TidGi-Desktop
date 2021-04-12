fc6d40dfd6d1eff80c94bff4478597449c66debd
tiddlygit-test <linonetwo012@gmail.com>
2021-04-12 23:17:19 +0800

    feat: description for token form

--- src/pages/AddWorkspace/Description.tsx
+++ src/pages/AddWorkspace/Description.tsx
@@ -8,15 +8,21 @@ import Switch from '@material-ui/core/Switch';
 import Typography from '@material-ui/core/Typography';
 
 const Container = styled(Paper)`
+  margin-top: 10px;
   padding: 10px;
 `;
 
-interface Props {
+/**
+ * Introduce difference between main and sub wiki.
+ * @returns
+ */
+export function MainSubWikiDescription({
+  isCreateMainWorkspace,
+  isCreateMainWorkspaceSetter,
+}: {
   isCreateMainWorkspace: boolean;
   isCreateMainWorkspaceSetter: (is: boolean) => void;
-}
-
-export function Description({ isCreateMainWorkspace, isCreateMainWorkspaceSetter }: Props): JSX.Element {
+}): JSX.Element {
   const { t } = useTranslation();
   const label = isCreateMainWorkspace ? t('AddWorkspace.MainWorkspace') : t('AddWorkspace.SubWorkspace');
   const description = isCreateMainWorkspace ? t('AddWorkspace.MainWorkspaceDescription') : t('AddWorkspace.SubWorkspaceDescription');
@@ -24,6 +30,33 @@ export function Description({ isCreateMainWorkspace, isCreateMainWorkspaceSetter
     <Container elevation={0} square>
       <FormControlLabel
         control={<Switch checked={isCreateMainWorkspace} onChange={(event) => isCreateMainWorkspaceSetter(event.target.checked)} />}
+        label={label}
+      />
+      <Typography variant="body2" display="inline">
+        {description}
+      </Typography>
+    </Container>
+  );
+}
+
+/**
+ * Introduce difference between Sync to cloud wiki and local wiki.
+ * @returns
+ */
+export function SyncedWikiDescription({
+  isCreateSyncedWorkspace,
+  isCreateSyncedWorkspaceSetter,
+}: {
+  isCreateSyncedWorkspace: boolean;
+  isCreateSyncedWorkspaceSetter: (is: boolean) => void;
+}): JSX.Element {
+  const { t } = useTranslation();
+  const label = isCreateSyncedWorkspace ? t('AddWorkspace.MainWorkspace') : t('AddWorkspace.SubWorkspace');
+  const description = isCreateSyncedWorkspace ? t('AddWorkspace.MainWorkspaceDescription') : t('AddWorkspace.SubWorkspaceDescription');
+  return (
+    <Container elevation={0} square>
+      <FormControlLabel
+        control={<Switch checked={isCreateSyncedWorkspace} onChange={(event) => isCreateSyncedWorkspaceSetter(event.target.checked)} />}
         label={label}
       />
       <Typography variant="body2" display="inline">
