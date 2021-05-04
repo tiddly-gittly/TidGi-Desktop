85b6d6d256f0143faa02675132e19ea62f151a8b
tiddlygit-test <linonetwo012@gmail.com>
2021-05-04 21:22:04 +0800

    chore: remove useless lproj

---
+++ scripts/afterPack.js
@@ -0,0 +1,31 @@
+/**
+ * Remove all .lproj files
+ * Based on https://ganeshrvel.medium.com/electron-builder-afterpack-configuration-5c2c986be665
+ * Adapted for electron forge https://github.com/electron-userland/electron-forge/issues/2248
+ */
+const path = require('path');
+const glob = require('glob');
+const fs = require('fs-extra');
+
+/**
+ * Specific which lproj you want to keep
+ */
+const keepingLprojRegEx = /(en)\.lproj/g;
+
+/**
+ *
+ * @param {*} buildPath /var/folders/qj/7j0zx32d0l75zmnrl1w3m3b80000gn/T/electron-packager/darwin-x64/TiddlyGit-darwin-x64/Electron.app/Contents/Resources/app
+ * @param {*} electronVersion 12.0.6
+ * @param {*} platform darwin
+ * @param {*} arch x64
+ * @returns
+ */
+exports.default = async (buildPath, electronVersion, platform, arch, callback) => {
+  const cwd = path.join(buildPath, '..');
+  const lproj = glob.sync('*.lproj', { cwd });
+  const pathsToRemove = lproj.filter((dir) => !keepingLprojRegEx.test(dir)).map((dir) => path.join(cwd, dir));
+  if (platform === 'darwin') {
+    await Promise.all(pathsToRemove.map((dir) => fs.remove(dir)));
+  }
+  callback();
+};
