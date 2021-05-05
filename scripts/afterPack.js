3dc50f75a1de9db72194d9ff130a05f514743068
tiddlygit-test <linonetwo012@gmail.com>
2021-05-05 14:56:10 +0800

    fix: language
    https://github.com/electron-userland/electron-forge/issues/2248

--- scripts/afterPack.js
+++ scripts/afterPack.js
@@ -10,7 +10,7 @@ const fs = require('fs-extra');
 /**
  * Specific which lproj you want to keep
  */
-const keepingLprojRegEx = /(en)\.lproj/g;
+const keepingLprojRegEx = /(en|zh_CN)\.lproj/g;
 
 /**
  *
