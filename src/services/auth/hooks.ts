06d18294945502c6551e2deb16dcd98f3fcb973b
tiddlygit-test <linonetwo012@gmail.com>
2021-06-03 01:23:59 +0800

    todo: user info not subscribed to update

--- src/services/auth/hooks.ts
+++ src/services/auth/hooks.ts
@@ -14,6 +14,7 @@ export function useStorageServiceUserInfo(serviceName: SupportedStorageServices)
   const [userInfo, userInfoSetter] = useState<IGitUserInfos | undefined>();
   useEffect(() => {
     void (async () => {
+      // TODO: should subscribe to update
       const newUserInfo = await window.service.auth.getStorageServiceUserInfo(serviceName);
       userInfoSetter(newUserInfo);
     })();
