b3b45db54a60da614e8c3c311a3d7618b9e14c42
tiddlygit-test <linonetwo012@gmail.com>
2021-07-07 20:16:40 +0800

    fix: custom ip implementation don't work well in windows

--- src/services/libs/url.ts
+++ src/services/libs/url.ts
@@ -1,32 +1,6 @@
-import { networkInterfaces } from 'os';
+import { address } from 'ip';
 import { defaultServerIP } from '@/constants/urls';
 import { logger } from './log';
-
-const nets = networkInterfaces();
-/**
- * Contains all available ip address.
- * ```js
- * { en0: [ '192.168.50.112' ] }
- * ```
- * @docs https://stackoverflow.com/questions/3653065/get-local-ip-address-in-node-js
- */
-export const ipAddresses: Record<string, string[]> = {};
-
-for (const name in nets) {
-  for (const net of nets[name]!) {
-    // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-    if (net.family === 'IPv4' && !net.internal) {
-      if (ipAddresses[name] === undefined) {
-        ipAddresses[name] = [];
-      }
-      ipAddresses[name].push(net.address);
-    }
-  }
-}
-
-export function getAvailableIPAddress(): string | undefined {
-  return ipAddresses.en0?.[0] ?? ipAddresses.eth0?.[0];
-}
 
 /**
  * get wiki address with local machine ip, so QR code will be correct, instead of get `0.0.0.0`
@@ -34,9 +8,9 @@ export function getAvailableIPAddress(): string | undefined {
  * @param originalUrl might be `"http://0.0.0.0:5212/"`
  */
 export function getLocalHostUrlWithActualIP(originalUrl: string): string {
-  const localHostUrlWithActualIP = originalUrl.replace(/(?:\d{1,3}\.){3}\d{1,3}/, getAvailableIPAddress() ?? defaultServerIP);
+  const localHostUrlWithActualIP = originalUrl.replace(/(?:\d{1,3}\.){3}\d{1,3}/, address('public') ?? defaultServerIP);
   logger.debug(
-    `Current available address: ${JSON.stringify(ipAddresses, undefined, '  ')}\nand getAvailableIPAddress() returns ${getAvailableIPAddress() ?? 'undefined'}
+    `Current available address: address() returns ${address('public') ?? 'undefined'}
     originalUrl: ${originalUrl} , localHostUrlWithActualIP ${localHostUrlWithActualIP}`,
   );
   return localHostUrlWithActualIP;
