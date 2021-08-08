2fa184423eb13f632070cb855d0ec11d2ce62bd6
林一二 <linonetwo012@gmail.com>
2021-08-08 17:29:06 +0800

    fix: git not found in dev

--- src/constants/appPaths.ts
+++ src/constants/appPaths.ts
@@ -7,4 +7,4 @@ export const SETTINGS_FOLDER = isDevelopmentOrTest
   ? path.resolve(sourcePath, '..', developmentSettingFolderName)
   : // eslint-disable-next-line @typescript-eslint/no-var-requires
     path.resolve(require('electron').app.getPath('userData'), 'settings');
-export const LOCAL_GIT_DIRECTORY = isDevelopmentOrTest ? undefined : path.resolve(process.resourcesPath, 'node_modules', 'dugite', 'git');
+export const LOCAL_GIT_DIRECTORY = path.resolve(isDevelopmentOrTest ? path.join(sourcePath, '..') : process.resourcesPath, 'node_modules', 'dugite', 'git');
