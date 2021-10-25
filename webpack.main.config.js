8c983ec5224adb43140abf9a0f08318bd21144d5
林一二 <linonetwo012@gmail.com>
2021-10-25 13:58:57 +0800

    Update webpack.main.config.js

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -7,6 +7,7 @@ const plugins = require('./webpack.plugins');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
+  target: 'electron-main',
   /**
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
