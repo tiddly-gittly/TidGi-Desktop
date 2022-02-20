ec2b524d432d108b8abbf274ef02cb7ed32b7648
林一二 <linonetwo012@gmail.com>
2022-02-20 18:51:50 +0800

    fix: gh config path

--- src/services/wiki/plugin/ghPages.ts
+++ src/services/wiki/plugin/ghPages.ts
@@ -8,13 +8,13 @@ interface IGhOptions {
 export async function updateGhConfig(wikiPath: string, options: IGhOptions): Promise<void> {
   // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
   if (options.branch && options.branch !== 'master') {
-    const ghPagesConfigPath = (path.join(wikiPath, '.github', 'gh-pages.yml'), 'utf8');
+    const ghPagesConfigPath = path.join(wikiPath, '.github', 'workflows', 'gh-pages.yml');
     try {
       const content = await fs.readFile(ghPagesConfigPath, 'utf8');
       const newContent = content.replace(/(branches:\n\s+- )(master)$/m, `$1${options.branch}`);
       await fs.writeFile(ghPagesConfigPath, newContent, 'utf8');
     } catch (error) {
-      logger.error(error);
+      logger.error(`updateGhConfig Error: ${(error as Error).message}`);
     }
   }
 }
