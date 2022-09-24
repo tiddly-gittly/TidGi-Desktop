1979ce92f39f813af6c27a90ebc9f58ee0d15d09
tiddlygit-test <linonetwo012@gmail.com>
2022-09-25 02:30:23 +0800

    fix: correct view height when no titlebar

--- src/services/libs/getViewBounds.ts
+++ src/services/libs/getViewBounds.ts
@@ -10,11 +10,11 @@ export default async function getViewBounds(
   width?: number,
 ): Promise<{ height: number; width: number; x: number; y: number }> {
   const preferencesService = container.get<IPreferenceService>(serviceIdentifier.Preference);
-  const showSidebar = await preferencesService.get('sidebar');
+  const [showSidebar, showTitleBar] = await Promise.all([preferencesService.get('sidebar'), preferencesService.get('titleBar')]);
 
   const x = showSidebar ? 68 : 0;
   /** add title bar height, move down 28px https://github.com/electron/electron/pull/34713 */
-  const y = isMac ? 28 : 0;
+  const y = isMac && showTitleBar ? 28 : 0;
 
   if (findInPage) {
     const FIND_IN_PAGE_HEIGHT = 42;
