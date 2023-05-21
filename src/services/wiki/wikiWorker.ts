17d88b650580a3dd333736091d41d2717ab0721d
linonetwo <linonetwo012@gmail.com>
2023-05-21 21:54:23 +0800

    docs: Electron failed to install correctly, please delete node_modules/electron and try installing again

--- src/services/wiki/wikiWorker.ts
+++ src/services/wiki/wikiWorker.ts
@@ -1,5 +1,7 @@
 /**
- * Worker environment is not part of electron environment, so don't import "@/constants/paths" here, as its process.resourcesPath will become undefined and throw Errors
+ * Worker environment is not part of electron environment, so don't import "@/constants/paths" here, as its process.resourcesPath will become undefined and throw Errors.
+ * 
+ * Don't use i18n and logger in worker thread. For example, 12b93020, will throw error "Electron failed to install correctly, please delete node_modules/electron and try installing again ...worker.js..."
  */
 import { uninstall } from '@/helpers/installV8Cache';
 import 'source-map-support/register';
