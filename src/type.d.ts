6ae0d4c4bdabae6e21d0104a3f943309dae78216
linonetwo <linonetwo012@gmail.com>
2023-06-03 19:42:47 +0800

    Update type.d.ts

--- src/type.d.ts
+++ src/type.d.ts
@@ -9,6 +9,27 @@ declare module '@tiddlygit/tiddlywiki' {
   export * from 'tiddlywiki';
 }
 
+declare module 'sqlite-vss' {
+  export interface Database {
+    // after https://deno.land/x/sqlite3@0.8.0/mod.ts?s=Database#method_loadExtension_0
+    loadExtension(file: string, entrypoint?: string | undefined): void;
+  }
+
+  /**
+   * Loads the sqlite-vss extension on the given sqlite3 database.
+   */
+  export function loadVector(database: Database): void;
+  /**
+   * Loads the sqlite-vss extension on the given sqlite3 database.
+   */
+  export function loadVss(database: Database): void;
+
+  /**
+   * Loads the sqlite-vss extension on the given sqlite3 database.
+   */
+  export function load(database: Database): void;
+}
+
 declare module 'threads-plugin' {
   const value: any;
   export default value;
