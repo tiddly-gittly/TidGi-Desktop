30fb258fa2935911529e3fd3c8677b3eb41c9164
lin onetwo <linonetwo012@gmail.com>
2023-06-09 00:37:12 +0800

    feat: use espree to guess variables user may need

--- src/services/wiki/plugin/zxPlugin.ts
+++ src/services/wiki/plugin/zxPlugin.ts
@@ -1,3 +1,6 @@
+/**
+ * Can't use logger in this file, because it runs in the worker.
+ */
 import type { ITiddlyWiki } from '@tiddlygit/tiddlywiki';
 import * as espree from 'espree';
 import _ from 'lodash';
@@ -34,8 +37,44 @@ export function executeScriptInTWContext(scriptContent: string, context: ITWVMCo
   return context.executionResults;
 }
 
+export interface EspreeASTRoot {
+  body: Body[];
+  sourceType: string;
+  type: string;
+}
+export interface Body {
+  declarations: EspreeASTVariableDeclarator[];
+  kind: string;
+  type: string;
+}
+export interface EspreeASTVariableDeclarator {
+  id: EspreeASTId;
+  type: string;
+}
+
+export interface EspreeASTId {
+  name: string;
+  type: string;
+}
+/**
+ * Extract variables from script content. We will use these variables to construct a `console.dir` that serialize all these variables, and deserialize them in next code context.
+ *
+ * Use https://esprima.org/demo/parse.html# as playground to see the AST.
+ * @param scriptContent string that is already separated by TW_SCRIPT_SEPARATOR.
+ * @returns variables that we need to extract, serialize, and deserialize, then send to next code context
+ * @url https://stackoverflow.com/a/25473571/4617295
+ */
 export function getVariablesFromScript(scriptContent: string): string[] {
-  espree.parse(scriptContent, { sourceType: 'module' });
+  try {
+    const tree = espree.parse(scriptContent, { sourceType: 'module' }) as EspreeASTRoot;
+    const topLevelVariables = tree.body.filter(node => node.type === 'VariableDeclaration' && node.declarations?.length > 0).flatMap(node =>
+      node.declarations.map(declaration => declaration.id.name)
+    );
+    return topLevelVariables;
+  } catch {
+    // Can't use logger in this file to log error, because it runs in the worker. Just return empty variable list, let user to guess...
+    return [];
+  }
 }
 
 export interface ITWVMContext {
