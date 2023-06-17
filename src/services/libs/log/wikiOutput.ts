46a6e334f456058f9425a084e1988f22d827b483
lin onetwo <linonetwo012@gmail.com>
2023-06-17 10:01:07 +0800

    refactor: make time costing wiki log to async

--- src/services/libs/log/wikiOutput.ts
+++ src/services/libs/log/wikiOutput.ts
@@ -2,24 +2,33 @@ import fs from 'fs-extra';
 import path from 'path';
 
 import { LOG_FOLDER } from '@/constants/appPaths';
+import { logger } from '.';
 
 export function getWikiLogFilePath(wikiName: string): string {
   const logFileName = wikiName.replaceAll(/["*/:<>?\\|]/g, '_');
   const logFilePath = path.join(LOG_FOLDER, `${logFileName}.log`);
   return logFilePath;
 }
-export function wikiOutputToFile(wikiName: string, message: string): void {
-  fs.appendFileSync(getWikiLogFilePath(wikiName), message);
+export async function wikiOutputToFile(wikiName: string, message: string) {
+  try {
+    await fs.appendFile(getWikiLogFilePath(wikiName), message);
+  } catch (error) {
+    logger.error(`${getWikiLogFilePath(wikiName)}: ${(error as Error).message})}`, { function: 'wikiOutputToFile' });
+  }
 }
 
 /**
  * Recreate log file
  * @param {string} wikiName
  */
-export function refreshOutputFile(wikiName: string): void {
-  const logFilePath = getWikiLogFilePath(wikiName);
-  if (fs.existsSync(logFilePath)) {
-    fs.removeSync(logFilePath);
+export async function refreshOutputFile(wikiName: string) {
+  try {
+    const logFilePath = getWikiLogFilePath(wikiName);
+    if (await fs.exists(logFilePath)) {
+      await fs.remove(logFilePath);
+    }
+    await fs.createFile(logFilePath);
+  } catch (error) {
+    logger.error(`${getWikiLogFilePath(wikiName)}: ${(error as Error).message})}`, { function: 'refreshOutputFile' });
   }
-  fs.createFileSync(logFilePath);
 }
