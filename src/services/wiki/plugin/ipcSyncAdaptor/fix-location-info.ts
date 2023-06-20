40f42e73111746a476c84527c6428b0dc34244b9
lin onetwo <linonetwo012@gmail.com>
2023-06-20 10:51:08 +0800

    feat: add tokenAuthHeader to info tiddler

--- src/services/wiki/plugin/ipcSyncAdaptor/fix-location-info.ts
+++ src/services/wiki/plugin/ipcSyncAdaptor/fix-location-info.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 
+import { getTidGiAuthHeaderWithToken } from '@/constants/auth';
 import { getDefaultHTTPServerIP } from '@/constants/urls';
 import type { WindowMeta, WindowNames } from '@services/windows/WindowProperties';
 
@@ -14,23 +16,38 @@ function getInfoTiddlerFields(updateInfoTiddlersCallback: (infos: Array<{ text: 
   const workspaceID = (window.meta as WindowMeta[WindowNames.view] | undefined)?.workspaceID;
   infoTiddlerFields.push({ title: '$:/info/tidgi', text: mapBoolean(isInTidGi) });
   if (isInTidGi && workspaceID) {
-    infoTiddlerFields.push({ title: '$:/info/tidgi-workspace-id', text: workspaceID });
+    infoTiddlerFields.push({ title: '$:/info/tidgi/workspaceID', text: workspaceID });
     void window.service.workspace.get(workspaceID).then(async (workspace) => {
       if (workspace === undefined) return;
+      const {
+        https = { enabled: false },
+        port,
+        enableHTTPAPI,
+        tokenAuth,
+        authToken,
+        userName,
+      } = workspace;
       const asyncInfoTiddlerFields: Array<{ text: string; title: string }> = [];
       const setLocationProperty = function(name: string, value: string) {
         asyncInfoTiddlerFields.push({ title: '$:/info/url/' + name, text: value });
       };
-      const localHostUrl = await window.service.native.getLocalHostUrlWithActualInfo(getDefaultHTTPServerIP(workspace.port), workspaceID);
+      const localHostUrl = await window.service.native.getLocalHostUrlWithActualInfo(getDefaultHTTPServerIP(port), workspaceID);
       const urlObject = new URL(localHostUrl);
       setLocationProperty('full', (localHostUrl).split('#')[0]);
       setLocationProperty('host', urlObject.host);
       setLocationProperty('hostname', urlObject.hostname);
-      setLocationProperty('protocol', workspace.https ? 'https' : 'http');
+      setLocationProperty('protocol', https ? 'https' : 'http');
       setLocationProperty('port', urlObject.port);
       setLocationProperty('pathname', urlObject.pathname);
       setLocationProperty('search', urlObject.search);
       setLocationProperty('origin', urlObject.origin);
+
+      infoTiddlerFields.push({ title: '$:/info/tidgi/tokenAuth', text: mapBoolean(tokenAuth) }, { title: '$:/info/tidgi/enableHTTPAPI', text: mapBoolean(enableHTTPAPI) });
+      if (tokenAuth) {
+        const fallbackUserName = await window.service.auth.get('userName');
+        const tokenAuthHeader = `"${getTidGiAuthHeaderWithToken(authToken ?? '')}": "${userName || fallbackUserName || ''}"`;
+        asyncInfoTiddlerFields.push({ title: '$:/info/tidgi/tokenAuthHeader', text: tokenAuthHeader });
+      }
       updateInfoTiddlersCallback(asyncInfoTiddlerFields);
     });
   }
