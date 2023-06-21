b654996ff609cce32fdd35fa025eb58525447017
linonetwo <linonetwo012@gmail.com>
2023-06-21 18:51:05 +0800

    fix: handle open file error, prevent crush

--- src/services/view/setupViewFileProtocol.ts
+++ src/services/view/setupViewFileProtocol.ts
@@ -1,11 +1,15 @@
+import { WikiChannel } from '@/constants/channels';
 import { whenCommonInitFinished } from '@/helpers/mainInitFinished';
 import { container } from '@services/container';
+import { i18n } from '@services/libs/i18n';
 import { logger } from '@services/libs/log';
 import { INativeService } from '@services/native/interface';
 import serviceIdentifier from '@services/serviceIdentifier';
+import type { IWikiService } from '@services/wiki/interface';
 import { BrowserView, shell } from 'electron';
 import fs from 'fs-extra';
 import { INewWindowAction } from './interface';
+import type { INewWindowContext } from './setupViewEventHandlers';
 
 /**
  * Handles in-wiki file link opening.
@@ -15,16 +19,25 @@ import { INewWindowAction } from './interface';
  *
  * For  file:/// in-app assets loading., see handleFileProtocol() in `src/services/native/index.ts`.
  */
-export function handleOpenFileExternalLink(nextUrl: string): INewWindowAction | undefined {
+export function handleOpenFileExternalLink(nextUrl: string, newWindowContext: INewWindowContext): INewWindowAction | undefined {
   const nativeService = container.get<INativeService>(serviceIdentifier.NativeService);
+  const wikiService = container.get<IWikiService>(serviceIdentifier.Wiki);
   const absoluteFilePath = nativeService.formatFileUrlToAbsolutePath(nextUrl);
   const fileStat = fs.statSync(absoluteFilePath);
   if (fileStat.isDirectory()) {
     logger.info(`Opening directory ${absoluteFilePath}`, { function: 'handleOpenFileExternalLink' });
-    void shell.openPath(absoluteFilePath);
+    void shell.openPath(absoluteFilePath).catch((error) => {
+      const message = i18n.t('Log.FailedToOpenDirectory', { path: absoluteFilePath, message: (error as Error).message });
+      logger.warn(message, { function: 'handleOpenFileExternalLink' });
+      wikiService.wikiOperation(WikiChannel.generalNotification, newWindowContext.workspace.id, message);
+    });
   } else if (fileStat.isFile()) {
     logger.info(`Opening file ${absoluteFilePath}`, { function: 'handleOpenFileExternalLink' });
-    void shell.openPath(absoluteFilePath);
+    void shell.openPath(absoluteFilePath).catch((error) => {
+      const message = i18n.t('Log.FailedToOpenFile', { path: absoluteFilePath, message: (error as Error).message });
+      logger.warn(message, { function: 'handleOpenFileExternalLink' });
+      wikiService.wikiOperation(WikiChannel.generalNotification, newWindowContext.workspace.id, message);
+    });
     // void shell.openExternal(absoluteFilePath);
   }
   return {
@@ -41,20 +54,26 @@ export function handleViewFileContentLoading(view: BrowserView) {
    * This function is called after `await app.whenReady()`, but electron will still throw error `Failed to register protocol: filefix` in https://github.com/tiddly-gittly/TidGi-Desktop/issues/422 , so we further delay it.
    */
   void whenCommonInitFinished().then(() => {
-    /**
-     * Electron's bug, file protocol is not handle-able, won't get any callback. But things like `filea://` `filefix` works.
-     */
-    view.webContents.session.protocol.handle('filefix', async (request) => {
-      let { pathname } = new URL(request.url);
-      pathname = decodeURIComponent(pathname);
-      logger.info(`Loading file content from ${pathname}`, { function: 'handleViewFileContentLoading view.webContents.session.protocol.handle' });
-      try {
-        const file = await fs.readFile(pathname);
-        return new Response(file, { status: 200 });
-      } catch (error) {
-        return new Response(undefined, { status: 404, statusText: (error as Error).message });
+    try {
+      if (!view.webContents.session.protocol.isProtocolHandled('filefix')) {
+        /**
+         * Electron's bug, file protocol is not handle-able, won't get any callback. But things like `filea://` `filefix` works.
+         */
+        view.webContents.session.protocol.handle('filefix', async (request) => {
+          let { pathname } = new URL(request.url);
+          pathname = decodeURIComponent(pathname);
+          logger.info(`Loading file content from ${pathname}`, { function: 'handleViewFileContentLoading view.webContents.session.protocol.handle' });
+          try {
+            const file = await fs.readFile(pathname);
+            return new Response(file, { status: 200 });
+          } catch (error) {
+            return new Response(undefined, { status: 404, statusText: (error as Error).message });
+          }
+        });
       }
-    });
+    } catch (error) {
+      logger.error(`Failed to register protocol: ${(error as Error).message}`, { function: 'handleViewFileContentLoading' });
+    }
   });
   view.webContents.session.webRequest.onBeforeRequest((details, callback) => {
     if (details.url.startsWith('file://') || details.url.startsWith('open://')) {
