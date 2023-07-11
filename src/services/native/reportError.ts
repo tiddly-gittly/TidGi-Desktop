5a6a9eabde87425f765bbcd225e8d74b84d398c1
lin onetwo <linonetwo012@gmail.com>
2023-07-12 00:04:14 +0800

    Update reportError.ts

--- src/services/native/reportError.ts
+++ src/services/native/reportError.ts
@@ -18,27 +18,24 @@ export function reportErrorToGithubWithTemplates(error: Error): void {
   openNewGitHubIssue({
     user: 'tiddly-gittly',
     repo: 'TidGi-Desktop',
-    template: 'bug.md',
+    template: 'bug.yml',
     title: `bug: ${(error.message ?? '').substring(0, 100)}`,
-    body: `## Environment
+    body: `### Environment 环境信息
 
 ${debugInfo()}
 
-## Description:
+### Description 描述
 
-<!-- Upload log file, Describe how the bug manifests and what the behavior would be without the bug. 上传 log 文件，描述该错误是如何表现出来的，以及在正常情况下应该有什么样的行为 -->
+${(error.message ?? '')}`
 
-## Steps to Reproduce:
+${(error.stack ?? '')}`
 
-<!--  Please explain the steps required to duplicate the issue, especially if you are able to provide a sample or a screen recording. 请解释复现该问题所需的步骤，有录屏最好。 -->
+### Steps to Reproduce 复现方式
 
-## Additional Context
 
-\`\`\`typescript\n${error.stack ?? 'No error.stack'}\n\`\`\`
 
----
+### Additional Context 额外上下文
 
-<!-- List any other information that is relevant to your issue. Stack traces, related issues, suggestions on how to add, use case, forum links, screenshots, OS if applicable, etc. 列出与你的问题有关的任何其他信息。报错堆栈、相关问题（issue）、关于如何添加的建议、使用案例、论坛链接、屏幕截图、操作系统（如果适用）等等。 -->
 
 `,
   });
