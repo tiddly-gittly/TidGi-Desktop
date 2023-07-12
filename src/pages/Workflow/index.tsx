097a7d58d7cdc013a9c5ffef524c7414c25156f3
linonetwo <linonetwo012@gmail.com>
2023-07-12 13:08:00 +0800

    fix: transform noflo component to the-graph library
    https://github.com/noflo/noflo/issues/1036
    https://github.com/noflo/noflo/issues/1035#issuecomment-1631164094
    https://github.com/flowhub/the-graph/issues/622
    https://github.com/noflo/noflo/issues/1037
    https://github.com/flowhub/the-graph/issues/216
    https://github.com/noflo/noflo-component-loader/issues/240

--- src/pages/Workflow/index.tsx
+++ src/pages/Workflow/index.tsx
@@ -2,18 +2,17 @@
 /* eslint-disable @typescript-eslint/promise-function-async */
 import { useThemeObservable } from '@services/theme/hooks';
 import { type Graph, loadJSON } from 'fbp-graph/lib/Graph';
-import { ComponentLoader } from 'noflo';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import type { IFBPLibrary } from 'the-graph';
 import { GraphEditor } from './GraphEditor';
+import { getBrowserComponentLibrary } from './library';
 import { photoboothJSON } from './photobooth.json';
 
 export default function Workflow(): JSX.Element {
   const { t } = useTranslation();
   const theme = useThemeObservable();
 
-  const [library, setLibrary] = useState<IFBPLibrary | undefined>();
   const [graph, setGraph] = useState<Graph | undefined>();
   useEffect(() => {
     void loadJSON(photoboothJSON).then(graph => {
@@ -21,22 +20,12 @@ export default function Workflow(): JSX.Element {
     });
   }, []);
   // load library bundled by webpack noflo-component-loader from installed noflo related npm packages
+  const [library, setLibrary] = useState<IFBPLibrary | undefined>();
   useEffect(() => {
-    const loader = new ComponentLoader('');
-    void loader.listComponents().then((componentList) => {
-      const libraryToLoad: IFBPLibrary = {};
-      Object.entries(componentList).forEach(([name, componentDefinitionRaw]) => {
-        if (typeof componentDefinitionRaw === 'string') {
-          // TODO: handle these ComponentDefinition types
-          return;
-        }
-        if ('getComponent' in componentDefinitionRaw) {
-          const componentDefinition = componentDefinitionRaw.getComponent();
-          libraryToLoad[name] = componentDefinition;
-        }
-      });
+    void (async () => {
+      const libraryToLoad = await getBrowserComponentLibrary();
       setLibrary(libraryToLoad);
-    });
+    })();
   }, []);
   return graph && library
     ? (
---
+++ src/pages/Workflow/library.ts
@@ -0,0 +1,94 @@
+import { Component, ComponentLoader, InPort } from 'noflo';
+import type BasePort from 'noflo/lib/BasePort';
+import type { IFBPLibrary, INoFloProtocolComponent, INoFloProtocolComponentPort, INoFloUIComponent, INoFloUIComponentPort } from 'the-graph';
+
+/**
+ * Covert FBP Protocol component to the-graph library component
+ * @url https://github.com/noflo/noflo-ui/blob/22d26eca71b52cd5181dd253f76bdb13c6bfecd4/src/runtime.js#L17C1-L33
+ */
+export function componentForLibrary(component: INoFloProtocolComponent): INoFloUIComponent {
+  return {
+    name: component.name,
+    icon: component.icon ?? 'cog',
+    description: component.description ?? '',
+    subgraph: component.subgraph ?? false,
+    inports: component.inPorts?.map(portForLibrary) ?? [],
+    outports: component.outPorts?.map(portForLibrary) ?? [],
+  };
+}
+
+/**
+ * Convert noflo instance return by `componentDefinition.getComponent()`, to the input of `componentForLibrary`
+ * @url https://github.com/noflo/noflo-runtime-base/blob/36ff1a439e1df9ad7611afd3cda908cce9e14fe2/src/protocol/Component.js#L186-L211
+ */
+export function componentToProtocolComponent(name: string, instance: Component): INoFloProtocolComponent {
+  const inPorts: INoFloProtocolComponentPort[] = [];
+  const outPorts: INoFloProtocolComponentPort[] = [];
+  // support old noflo.Component that put BasePort directly in inPorts https://github.com/noflo/noflo/blob/6187566f761a463af95dd186de55dc488e2f03b8/src/lib/Component.js#L314
+  const inPortInstances = instance.inPorts.ports ?? instance.inPorts as unknown as BasePort[];
+  const outPortInstances = instance.outPorts.ports ?? instance.outPorts as unknown as BasePort[];
+  Object.keys(inPortInstances).forEach((portName) => {
+    const port = inPortInstances[portName];
+    if (port === undefined || (typeof port === 'function') || !port.canAttach()) return;
+    inPorts.push(processPort(portName, port));
+  });
+  Object.keys(outPortInstances).forEach((portName) => {
+    const port = outPortInstances[portName];
+    if (port === undefined || (typeof port === 'function') || !port.canAttach()) return;
+    outPorts.push(processPort(portName, port));
+  });
+
+  const icon = instance?.getIcon() ?? 'gear';
+
+  return {
+    name,
+    description: instance.description,
+    subgraph: instance.isSubgraph(),
+    icon,
+    inPorts,
+    outPorts,
+  };
+}
+
+function processPort(portName: string, port: BasePort): INoFloProtocolComponentPort {
+  /* eslint-disable @typescript-eslint/strict-boolean-expressions */
+  /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+  /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
+  // Required port properties
+  const portSerialized: INoFloProtocolComponentPort = {
+    id: portName,
+    type: (port?.getDataType?.() as string | undefined) ?? 'all',
+    schema: port?.getSchema?.() as string | undefined,
+    required: port?.isRequired?.() ?? false,
+    default: ((port as InPort)?.hasDefault?.() ?? false) ? port.options.default : undefined,
+    description: port.getDescription(),
+    values: port.options.values,
+  };
+  return portSerialized;
+}
+
+function portForLibrary(port: INoFloProtocolComponentPort): INoFloUIComponentPort {
+  return {
+    ...port,
+    name: port.id,
+    description: port.description || port.type,
+  };
+}
+
+export async function getBrowserComponentLibrary() {
+  const loader = new ComponentLoader('');
+  const componentList = await loader.listComponents();
+  const libraryToLoad: IFBPLibrary = {};
+  Object.entries(componentList).forEach(([name, componentDefinitionRaw]) => {
+    if (typeof componentDefinitionRaw === 'string') {
+      // TODO: handle these ComponentDefinition types
+      return;
+    }
+    if ('getComponent' in componentDefinitionRaw) {
+      const componentInstance = componentDefinitionRaw.getComponent();
+      libraryToLoad[name] = componentForLibrary(componentToProtocolComponent(name, componentInstance));
+    }
+  });
+  return libraryToLoad;
+}
