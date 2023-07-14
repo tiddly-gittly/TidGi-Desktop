abf531dc505c8933d9dc3f9f9047a150dd75d955
linonetwo <linonetwo012@gmail.com>
2023-07-14 23:12:26 +0800

    fix: if a page is also active, don't show it as active because it is hidden

--- src/pages/Main/useInitialPage.ts
+++ src/pages/Main/useInitialPage.ts
@@ -1,4 +1,6 @@
+/* eslint-disable unicorn/no-null */
 import { usePromiseValue } from '@/helpers/useServiceValue';
+import { PageType } from '@services/pages/interface';
 import { WindowNames } from '@services/windows/WindowProperties';
 import { useEffect, useState } from 'react';
 import { useLocation } from 'wouter';
@@ -6,13 +8,21 @@ import { useLocation } from 'wouter';
 export function useInitialPage() {
   const [, setLocation] = useLocation();
   // when first open the TidGi and no workspace is active (so no BrowserView will be on top of the React), goto the active pages route
-  const initialActivePage = usePromiseValue(async () => await window.service.pages.getActivePage());
+  const initialActivePage = usePromiseValue(async () => await window.service.pages.getActivePage(), null);
+  const initialActiveWorkspace = usePromiseValue(async () => await window.service.workspace.getActiveWorkspace(), null);
   // only do this once, and not triggering unnecessary rerender by using ref.
   const [alreadyInitialized, alreadyInitializedSetter] = useState(false);
   useEffect(() => {
-    if (initialActivePage !== undefined && !alreadyInitialized) {
-      setLocation(`/${WindowNames.main}/${initialActivePage.type}/`);
+    // active workspace has priority to show, so if a page is also active in settings, don't set it as active because it is hidden
+    if (initialActivePage !== null && initialActiveWorkspace !== null && !alreadyInitialized) {
+      if (initialActiveWorkspace === undefined) {
+        if (initialActivePage !== undefined) {
+          setLocation(`/${WindowNames.main}/${initialActivePage.type}/`);
+        }
+      } else {
+        setLocation(`/${WindowNames.main}/${PageType.wiki}/${initialActiveWorkspace.id}/`);
+      }
       alreadyInitializedSetter(true);
     }
-  }, [setLocation, initialActivePage, alreadyInitialized, alreadyInitializedSetter]);
+  }, [setLocation, initialActivePage, alreadyInitialized, alreadyInitializedSetter, initialActiveWorkspace]);
 }
