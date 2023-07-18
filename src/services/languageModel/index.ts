9971a1c38d33abeb048bf279c3ca12685c52e016
linonetwo <linonetwo012@gmail.com>
2023-07-18 22:59:00 +0800

    v0.8.1-prerelease5

--- src/services/languageModel/index.ts
+++ src/services/languageModel/index.ts
@@ -125,14 +125,10 @@ export class LanguageModel implements ILanguageModelService {
         const texts = { timeout: i18n.t('LanguageModel.GenerationTimeout') };
         switch (runner) {
           case LanguageModelRunner.llamaCpp: {
-            // DEBUG: console modelPath
-            console.log(`modelPath`, modelPath);
             observable = worker.runLLama({ completionOptions, loadConfig: { modelPath, ...config }, conversationID }, texts);
             break;
           }
           case LanguageModelRunner.rwkvCpp: {
-            // DEBUG: console RWKV_CPP_TOKENIZER_PATH
-            console.log(`RWKV_CPP_TOKENIZER_PATH`, RWKV_CPP_TOKENIZER_PATH);
             observable = worker.runRwkv({ completionOptions, loadConfig: { modelPath, tokenizerPath: RWKV_CPP_TOKENIZER_PATH, ...config }, conversationID }, texts);
             break;
           }
