26cf19542d1b207fe9ee2ea70c1e5c00243c9f9a
linonetwo <linonetwo012@gmail.com>
2023-08-17 19:43:36 +0800

    feat: in dev mode, set userData to a different folder, so gotTheLock will be true, we can run dev instance and normal instance.

--- src/constants/appPaths.ts
+++ src/constants/appPaths.ts
@@ -5,21 +5,19 @@ import { isDevelopmentOrTest } from './environment';
 import { cacheDatabaseFolderName, httpsCertKeyFolderName, languageModelFolderName, settingFolderName } from './fileNames';
 import { sourcePath } from './paths';
 
+// in dev mode, set userData to a different folder, so gotTheLock will be true, we can run dev instance and normal instance.
+if (isDevelopmentOrTest) {
+  app.setPath('userData', path.resolve(sourcePath, '..', 'userData-dev'));
+}
 export const USER_DATA_FOLDER = app.getPath('userData');
-export const SETTINGS_FOLDER = isDevelopmentOrTest
-  /** Used to store settings during dev and testing */
-  ? path.resolve(sourcePath, '..', `${settingFolderName}-dev`)
-  : path.resolve(USER_DATA_FOLDER, settingFolderName);
-export const HTTPS_CERT_KEY_FOLDER = isDevelopmentOrTest
-  ? path.resolve(sourcePath, '..', `${httpsCertKeyFolderName}-dev`)
-  : path.resolve(USER_DATA_FOLDER, httpsCertKeyFolderName);
-export const CACHE_DATABASE_FOLDER = isDevelopmentOrTest
-  ? path.resolve(sourcePath, '..', `${cacheDatabaseFolderName}-dev`)
-  : path.resolve(USER_DATA_FOLDER, cacheDatabaseFolderName);
+export const SETTINGS_FOLDER = path.resolve(USER_DATA_FOLDER, settingFolderName);
+export const HTTPS_CERT_KEY_FOLDER = path.resolve(USER_DATA_FOLDER, httpsCertKeyFolderName);
+export const CACHE_DATABASE_FOLDER = path.resolve(USER_DATA_FOLDER, cacheDatabaseFolderName);
+/** During dev, we don't want to clean up the language model folder */
 export const LANGUAGE_MODEL_FOLDER = isDevelopmentOrTest
   ? path.resolve(sourcePath, '..', `${languageModelFolderName}-dev`)
   : path.resolve(USER_DATA_FOLDER, languageModelFolderName);
 export const LOCAL_GIT_DIRECTORY = path.resolve(isDevelopmentOrTest ? path.join(sourcePath, '..') : process.resourcesPath, 'node_modules', 'dugite', 'git');
-export const LOG_FOLDER = isDevelopmentOrTest ? path.resolve(sourcePath, '..', 'logs') : path.resolve(USER_DATA_FOLDER, 'logs');
+export const LOG_FOLDER = path.resolve(USER_DATA_FOLDER, 'logs');
 export const V8_CACHE_FOLDER = v8CompileCacheLibrary.getCacheDir();
 export const DEFAULT_DOWNLOADS_PATH = path.join(app.getPath('home'), 'Downloads');
--- src/helpers/singleInstance.ts
+++ src/helpers/singleInstance.ts
@@ -1,6 +1,9 @@
 import { logger } from '@services/libs/log';
 import { app } from 'electron';
 
+/**
+ * Will return false if another instance with same `userData` path is already running. So we set different userData for dev and prod mode in `src/constants/appPaths.ts`, so you can open production tidgi during dev tidgi.
+ */
 const gotTheLock = app.requestSingleInstanceLock();
 
 logger.info('App booting');
