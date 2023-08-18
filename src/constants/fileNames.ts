75416acddca4ff8c051814a93049c83ea874ae02
linonetwo <linonetwo012@gmail.com>
2023-08-18 14:44:52 +0800

    feat: put wiki in repo folder, so it won't removed after restart

--- src/constants/fileNames.ts
+++ src/constants/fileNames.ts
@@ -3,7 +3,7 @@ export const httpsCertKeyFolderName = 'https-keys';
 export const cacheDatabaseFolderName = 'cache-database';
 export const languageModelFolderName = 'language-model';
 /** Used to place mock wiki during dev and testing */
-export const developmentWikiFolderName = 'tidgi-dev';
+export const developmentWikiFolderName = 'wiki-dev';
 export const localizationFolderName = 'localization';
 
 export const wikiPictureExtensions = ['jpg', 'jpeg', 'png', 'gif', 'tiff', 'tif', 'bmp', 'dib'];
