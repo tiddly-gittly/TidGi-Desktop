128d50a31bdd939cdbe6b1a14c4ba425cc899a0e
linonetwo <linonetwo012@gmail.com>
2023-12-28 19:08:48 +0800

    refactor: replace authing with oidc-client-ts

--- src/components/TokenForm/gitTokenHooks.ts
+++ src/components/TokenForm/gitTokenHooks.ts
@@ -1,55 +1,69 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
-import { APP_DOMAIN, APP_ID } from '@/constants/auth';
+import { IGitUserInfosWithoutToken } from '@services/git/interface';
 import { SupportedStorageServices } from '@services/types';
-import { AuthenticationClient } from 'authing-js-sdk';
-import { useCallback, useMemo } from 'react';
+import { UserManager, WebStorageStateStore } from 'oidc-client-ts';
+import { useCallback, useEffect, useState } from 'react';
 
-export function useAuth(storageService: SupportedStorageServices): [() => Promise<void>, () => Promise<void>] {
-  const authing = useMemo(
-    () =>
-      new AuthenticationClient({
-        appId: APP_ID,
-        appHost: APP_DOMAIN,
-      }),
-    [],
-  );
+export function useAuth(storageService: SupportedStorageServices, userInfo: IGitUserInfosWithoutToken): [() => Promise<void>, () => Promise<void>] {
+  const [userManager, setUserManager] = useState<UserManager>();
 
-  const onFailure = useCallback((error: Error) => {
-    console.error(error);
-  }, []);
-  const onClickLogout = useCallback(async () => {
-    await authing.logout();
-    await window.service.window.clearStorageData();
-  }, [authing]);
+  useEffect(() => {
+    void window.service.context.get('LOGIN_REDIRECT_PATH').then( (LOGIN_REDIRECT_PATH) => {
+      const settings = {
+        authority: 'https://github.com/',
+        client_id: userInfo.gitUserName,
+        redirect_uri: LOGIN_REDIRECT_PATH,
+        response_type: 'code',
+        scope: 'openid',
+        // post_logout_redirect_uri: '<YOUR_POST_LOGOUT_REDIRECT_URI>',
+        userStore: new WebStorageStateStore({ store: window.localStorage }),
+      };
+
+      const userManager = new UserManager(settings);
+      setUserManager(userManager);
+
+      userManager.events.addUserLoaded(async user => {
+        // Handle the loaded user information here
+        // Save the access token and other user details
+        if (user.access_token) {
+          await window.service.auth.set(`${storageService}-token`, user.access_token);
+        }
+        if (userInfo.gitUserName) {
+          await window.service.auth.set(`${storageService}-userName`, userInfo.gitUserName);
+        }
+        if (userInfo.email) {
+          await window.service.auth.set(`${storageService}-email`, userInfo.email);
+        }
+        // Additional user information might be available in the user.profile object
+      });
+
+      userManager.events.addSilentRenewError(error => {
+        console.error('Silent renew error', error);
+      });
+
+      // userManager.events.addUserSignedOut(async () => {
+      //   // Handle user sign-out event
+      //   await window.service.window.clearStorageData();
+      // });
+    });
+  }, [storageService, userInfo.email, userInfo.gitUserName]);
 
   const onClickLogin = useCallback(async () => {
-    // clear token first, otherwise github login window won't give us a chance to see the form
-    // void this.auth.logout();
-    // window.remote.clearStorageData();
     try {
-      await authing.social.authorize(storageService, {
-        onSuccess: async (user) => {
-          const thirdPartyIdentity = user.identities?.find((identity) => identity?.provider === storageService);
-          if (thirdPartyIdentity) {
-            if (thirdPartyIdentity.accessToken) {
-              await window.service.auth.set(`${storageService}-token`, thirdPartyIdentity.accessToken);
-            }
-            if (user.username) {
-              await window.service.auth.set(`${storageService}-userName`, user.username);
-            }
-            if (user.email) {
-              await window.service.auth.set(`${storageService}-email`, user.email);
-            }
-          }
-        },
-        onError: (code, message) => {
-          onFailure(new Error(message + String(code)));
-        },
-      });
+      await userManager?.signinRedirect();
     } catch (error) {
-      onFailure(error as Error);
+      console.error(error);
     }
-  }, [authing.social, onFailure, storageService]);
+  }, [userManager]);
+
+  const onClickLogout = useCallback(async () => {
+    try {
+      await userManager?.signoutRedirect();
+      await window.service.window.clearStorageData();
+    } catch (error) {
+      console.error(error);
+    }
+  }, [userManager]);
 
   return [onClickLogin, onClickLogout];
 }
