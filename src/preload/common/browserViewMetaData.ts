cdea2e9be911ee905052623e7b419e72d428ab7b
lin onetwo <linonetwo012@gmail.com>
2025-06-14 01:49:24 +0800

    Update browserViewMetaData.ts

--- src/preload/common/browserViewMetaData.ts
+++ src/preload/common/browserViewMetaData.ts
@@ -5,7 +5,7 @@ import { contextBridge, ipcRenderer } from 'electron';
 const metaDataArguments = process.argv
   .filter((item) => item.startsWith(MetaDataChannel.browserViewMetaData))
   .map((item) => item.replace(MetaDataChannel.browserViewMetaData, ''));
-export const windowName = (metaDataArguments[0] as WindowNames) ?? WindowNames.main;
+export const windowName = (metaDataArguments[0] as WindowNames | undefined) ?? WindowNames.main;
 const extraMetaJSONString = decodeURIComponent(metaDataArguments[1] ?? '{}');
 let extraMeta: WindowMeta[WindowNames] = {};
 try {
@@ -18,12 +18,12 @@ try {
 
 export let browserViewMetaData = { windowName, ...extraMeta };
 contextBridge.exposeInMainWorld('meta', () => browserViewMetaData);
-ipcRenderer.on(MetaDataChannel.getViewMetaData, (event, payload?: { ipcToken: string }) => {
+ipcRenderer.on(MetaDataChannel.getViewMetaData, (_event, payload?: { ipcToken: string }) => {
   ipcRenderer.send(`${MetaDataChannel.getViewMetaData}-${payload?.ipcToken ?? ''}`, browserViewMetaData);
 });
 /**
  * Receive update or windowMeta from server service.
  */
-ipcRenderer.on(MetaDataChannel.pushViewMetaData, (event, payload?: IPossibleWindowMeta) => {
+ipcRenderer.on(MetaDataChannel.pushViewMetaData, (_event, payload?: IPossibleWindowMeta) => {
   browserViewMetaData = { ...browserViewMetaData, ...payload };
 });
