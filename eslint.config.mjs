5944295133cd726fb4c72cad564a26f0d58600cc
linonetwo <linonetwo012@gmail.com>
2025-07-19 21:39:10 +0800

    fix: ConfigError: Config (unnamed): Key "overrides": This appears to be in eslintrc format rather than flat config format.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,14 +17,12 @@ export default [
       },
     },
     rules: {},
-    overrides: [
-      {
-        files: ['**/*.test.ts', '**/*.test.tsx', '**/*.spec.ts', '**/*.spec.tsx'],
-        rules: {
-          '@typescript-eslint/unbound-method': 'off',
-          'unicorn/prevent-abbreviations': 'off',
-        },
-      },
-    ],
-  }
+  },
+  {
+    files: ['**/*.test.ts', '**/*.test.tsx', '**/*.spec.ts', '**/*.spec.tsx'],
+    rules: {
+      '@typescript-eslint/unbound-method': 'off',
+      'unicorn/prevent-abbreviations': 'off',
+    },
+  },
 ];
