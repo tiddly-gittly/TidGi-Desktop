73790b33d9b9930c707d476b3faf3319cfc16a50
linonetwo <linonetwo012@gmail.com>
2025-12-13 17:37:14 +0800

    fix: Keep i18n ally think these keys exist, otherwise it will delete them during "check usage"

--- src/constants/defaultTiddlerNames.ts
+++ src/constants/defaultTiddlerNames.ts
@@ -1,1 +1,7 @@
+import { t } from '@services/libs/i18n/placeholder';
+
 export const rootTiddlers = ['$:/core/save/all', '$:/core/save/lazy-images', '$:/core/save/lazy-all'];
+// Keep i18n ally think these keys exist, otherwise it will delete them during "check usage"
+t('EditWorkspace.WikiRootTiddlerItems.all');
+t('EditWorkspace.WikiRootTiddlerItems.lazy-images');
+t('EditWorkspace.WikiRootTiddlerItems.lazy-all');
