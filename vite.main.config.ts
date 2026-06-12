1f78d0f8466fdf829ebe292166a7acd0fa605426
lin onetwo <linonetwo012@gmail.com>
2026-06-12 21:08:12 +0800

    fix react-ui templating issues, remove extraneous tsconfig aliases

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -58,7 +58,6 @@ export default defineConfig({
         'default-gateway',
         'electron-unhandled',
         'rotating-file-stream',
-        'expo-sqlite',
         ...typeormOptionalDepsRegex,
       ],
     },
